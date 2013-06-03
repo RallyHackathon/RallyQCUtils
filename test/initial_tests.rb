3f04240ab3bf2a37d36a4c8b67031017af01fb40
Michael Toback <mtoback@vmware.com>
2013-06-03 08:05:09 -0600

    added test for qc_xml

--- test/initial_tests.rb
+++ test/initial_tests.rb
@@ -24,15 +24,23 @@ class InitialTests < Test::Unit::TestCase
     assert_equal("0.1.0", RallyQCUtils::VERSION )
   end
   
+  
+ def test_qc_xml
+   qc_info = {"Domain"=>"domain1","Project"=>"project1"}
+   config_writer =  RallyQCUtils::ConfigWriter.new
+   qc_xml = config_writer.construct_qc_xml(qc_info)
+   parser = Nori.new(:parser => :rexml)
+   loaded_config = parser.parse(qc_xml)
+   assert_equal("domain1",loaded_config["QCConnection"]['Domain'])
+   assert_equal("project1", loaded_config["QCConnection"]['Project'])
+ end
  def test_rally_xml
    rally_info = {"WorkspaceName"=>"workspace1", "Projects"=>['project1']}
    config_writer = RallyQCUtils::ConfigWriter.new
    rally_xml = config_writer.construct_rally_xml(rally_info)
-   puts rally_xml
    parser = Nori.new(:parser => :rexml)
    loaded_config = parser.parse(rally_xml)
    assert_equal("workspace1",loaded_config["RallyConnection"]['WorkspaceName'])
    assert_equal("project1", loaded_config["RallyConnection"]['Projects']['Project'])
-   assert_equal(1,1)
  end
 end
